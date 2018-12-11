f5d15648d41920ed90f90b48add2cb5df0bf3ff6
Sewef <elagroum@gmail.com>
2018-12-11 14:59:06 +0100

    Update FeatureCharms.java

--- src/main/java/net/darkhax/darkutils/features/charms/FeatureCharms.java
+++ src/main/java/net/darkhax/darkutils/features/charms/FeatureCharms.java
@@ -99,8 +99,8 @@ public class FeatureCharms extends Feature {
             if (entityBase instanceof EntityPlayer && itemFocusSash.hasItem(entityBase) && entityBase.getHealth() >= entityBase.getMaxHealth()) {
                 
                 float damage = event.getAmount();
-                damage = event.getEntityLiving().applyArmorCalculations(event.getSource(), damage);
-                damage = event.getEntityLiving().applyPotionDamageCalculations(event.getSource(), damage);
+                damage = new entityBase.applyArmorCalculations(event.getSource(), damage);
+                damage = new entityBase.applyPotionDamageCalculations(event.getSource(), damage);
                 
                 float maxHealth = entityBase.getMaxHealth();
                 maxHealth += entityBase.getAbsorptionAmount();
